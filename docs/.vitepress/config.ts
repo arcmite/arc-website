ae4dc16f0c3945f34bb0169fd5b18a245ab76374
haz <122119697+creacher4@users.noreply.github.com>
2025-06-12 23:31:44 +0100

    fix: Correct base path for GitHub Pages deployment

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,61 +1,61 @@
-import { defineConfig } from 'vitepress';
+import { defineConfig } from "vitepress";
 
 export default defineConfig({
-  lang: 'en-US',
-  title: 'ARC',
-  description: 'A start-to-finish modding reference for Assetto Corsa.',
+  lang: "en-US",
+  title: "ARC",
+  description: "A start-to-finish modding reference for Assetto Corsa.",
 
   // BASE LINK FOR GITHUB PAGES
-  base: 'assetto-corsa-arc/',
+  base: "/assetto-corsa-arc/",
 
-  head: [['link', { rel: 'stylesheet', href: '/style.css' }]],
+  head: [["link", { rel: "stylesheet", href: "/style.css" }]],
 
   themeConfig: {
     // The main navigation bar at the top of the page
     nav: [
-      { text: 'Guides', link: '/guides/' },
-      { text: 'Creators', link: '/creators/' },
-      { text: 'Wiki', link: '/wiki/' },
-      { text: 'About', link: '/about' },
+      { text: "Guides", link: "/guides/" },
+      { text: "Creators", link: "/creators/" },
+      { text: "Wiki", link: "/wiki/" },
+      { text: "About", link: "/about" },
     ],
 
     // The sidebar configuration. VitePress automatically shows the
     // correct sidebar based on the user's current location.
     sidebar: {
       // This sidebar will be shown on all pages under the /guides/ path
-      '/guides/': [
+      "/guides/": [
         {
-          text: 'Installation & Setup',
-          items: [{ text: 'Getting Started', link: '/guides/getting-started' }],
+          text: "Installation & Setup",
+          items: [{ text: "Getting Started", link: "/guides/getting-started" }],
         },
         {
-          text: 'Modding',
+          text: "Modding",
           items: [
-            { text: 'Installing Mods', link: '/guides/installing-mods' },
-            { text: 'Photo Mode', link: '/guides/photo-mode' },
+            { text: "Installing Mods", link: "/guides/installing-mods" },
+            { text: "Photo Mode", link: "/guides/photo-mode" },
           ],
         },
       ],
       // This sidebar will be shown on all pages under the /creators/ path
-      '/creators/': [
+      "/creators/": [
         {
-          text: 'Creators & Modders',
+          text: "Creators & Modders",
           items: [
-            { text: 'Ilja Jusupov', link: '/creators/ilja-jusupov' },
-            { text: 'Peter Boese', link: '/creators/peter-boese' },
-            { text: 'Foo-Bar', link: '/creators/foo-bar' },
+            { text: "Ilja Jusupov", link: "/creators/ilja-jusupov" },
+            { text: "Peter Boese", link: "/creators/peter-boese" },
+            { text: "Foo-Bar", link: "/creators/foo-bar" },
           ],
         },
       ],
       // This sidebar will be shown on all pages under the /wiki/ path
-      '/wiki/': [
+      "/wiki/": [
         {
-          text: 'Wiki',
+          text: "Wiki",
           items: [
-            { text: 'Custom Shaders Patch', link: '/wiki/csp' },
-            { text: 'Look Up Tables', link: '/wiki/lut' },
-            { text: 'Post-Processing Filters', link: '/wiki/ppfilter' },
-            { text: 'Pure', link: '/wiki/pure' },
+            { text: "Custom Shaders Patch", link: "/wiki/csp" },
+            { text: "Look Up Tables", link: "/wiki/lut" },
+            { text: "Post-Processing Filters", link: "/wiki/ppfilter" },
+            { text: "Pure", link: "/wiki/pure" },
           ],
         },
       ],
@@ -63,21 +63,21 @@ export default defineConfig({
 
     socialLinks: [
       {
-        icon: 'github',
-        link: 'https://github.com/creacher4/assetto-corsa-arc',
+        icon: "github",
+        link: "https://github.com/creacher4/assetto-corsa-arc",
       },
-      { icon: 'discord', link: 'https://discord.gg/Szd4nu4wEp' },
+      { icon: "discord", link: "https://discord.gg/Szd4nu4wEp" },
     ],
 
     footer: {
-      message: 'Built with VitePress.',
-      copyright: 'Copyright © 2025 | The ARC Team',
+      message: "Built with VitePress.",
+      copyright: "Copyright © 2025 | The ARC Team",
     },
 
     editLink: {
       pattern:
-        'https://github.com/creacher4/assetto-corsa-arc/edit/main/docs/:path',
-      text: 'Edit this page on GitHub',
+        "https://github.com/creacher4/assetto-corsa-arc/edit/main/docs/:path",
+      text: "Edit this page on GitHub",
     },
 
     // The 'lastUpdated' feature requires Git. It is disabled here to prevent
@@ -86,7 +86,7 @@ export default defineConfig({
 
     // Enables local full-text search
     search: {
-      provider: 'local',
+      provider: "local",
     },
   },
 });
