6ad9541d7c729b680d464060a0e49bf18faa5504
lint069 <ljovic009@gmail.com>
2025-08-09 00:05:55 +0200

    docs(showcase): add some new photos

--- docs/showcase/data.ts
+++ docs/showcase/data.ts
@@ -32,6 +32,84 @@ export const items = [
     author: "@lint0",
     details: "PPFilter: C13 | Car: Porsche 911 GT3 RS | Track: N/A",
   },
+  {
+    src: "/images/showcase/trawa_rx7_spoiler.png",
+    alt: "Trawa mazda fd3s doristyle",
+    author: "@lint0",
+    details: "PPFilter: C13 | Car: Mazda FD3S Dori Style | Track: Shfy Studio",
+  },
+  {
+    src: "/images/showcase/loading_2025.png",
+    alt: "yamainu on srp",
+    author: "@𝔃𝖆𝖓†",
+    details: "PPFilter: Revolution | Car: Porsche 924 'The Yamainu' | Track: Shutoko Revival Project",
+  },
+  {
+    src: "/images/showcase/honda_nsx_na2.png",
+    alt: "Honda-NSX-NA2",
+    author: "N/A",
+    details: "PPFilter: N/A | Car: Honda NSX NA2 | Track: N/A",
+  },
+  {
+    src: "/images/showcase/trawa_rx7_blue.png",
+    alt: "Trawa mazda fd3s doristyle blue",
+    author: "N/A",
+    details: "PPFilter: N/A | Car: Mazda FD3S Dori Style | Track: N/A",
+  },
+  {
+    src: "/images/showcase/emil_drift.png",
+    alt: "Emil tire smoke",
+    author: "@Emil",
+    details: "PPFilter: N/A | Car: Nissan Silvia S13 | Track: N/A",
+  },
+  {
+    src: "/images/showcase/bielm_srp.png",
+    alt: "Porsche on srp",
+    author: "@BielM",
+    details: "PPFilter: Newgen | Car: Porsche 911 GT3 RS | Track: Shutoko Revival Project",
+  },
+  {
+    src: "/images/showcase/bielm_porsche.png",
+    alt: "BielM",
+    author: "@BielM",
+    details: "PPFilter: Newgen | Car: Porsche 911 GT3 RS | Track: N/A",
+  },
+  {
+    src: "/images/showcase/marco_pagani.png",
+    alt: "pagani in showroom",
+    author: "@marco",
+    details: "PPFilter: N/A | Car: Pagani Zonda F | Track: J&G Small Studio",
+  },
+  {
+    src: "/images/showcase/zaentyt_nsx.png",
+    alt: "",
+    author: "@zaentyt",
+    details: "PPFilter: Z_BWCine | Car: Honda NSX Veilside | Track: Domi Waves",
+  },
+  {
+    src: "/images/showcase/zaentyt_aegis.png",
+    alt: "aegis visuals banner",
+    author: "@zaentyt",
+    details: "PPFilter: Z_GT | Car: Ferrari F355 Challenge Persephone | Track: Z Jpn Garage",
+  },
+  {
+    src: "/images/showcase/zaentyt_yellow.png",
+    alt: "Le Mans",
+    author: "@zaentyt",
+    details: "PPFilter: Z_GTCine | Car: LM Hypercar V Series R | Track: RT Daytona",
+  },
+  {
+    src: "/images/showcase/zaentyt_s2000.png",
+    alt: "Mirror",
+    author: "@zaentyt",
+    details: "PPFilter: Z_GTCine | Car: Honda S2000 GT1 Amuse | Track: Bst Auto Salon",
+  },
+  {
+    src: "/images/showcase/zaentyt_senna.png",
+    alt: "colorful",
+    author: "@zaentyt",
+    details: "PPFilter: Z_GTCine | Car: Mclaren Senna | Track: Rio",
+  },
 
   // To add a new image, just add a new entry here with the same structure
 ];
